089a4b00f7b4290c91baf91524ee6dec46f30b0f
Jon Nordby <jononor@gmail.com>
2026-07-18 08:47:48 +0200

    tests: Check UCI occupancy detection dataset .zip
    Currently failing to decompress

--- tests/test_zipfile.py
+++ tests/test_zipfile.py
@@ -11,6 +11,19 @@ def test_simple_roundtrip_stored():
 def test_simple_roundtrip_deflate():
     check_roundtrip_simple(ZIP_DEFLATED, 9)
 
+def test_uci_occupancy_dataset():
+    # regression test. Would fail with BadZipFile: Bad CRC-32
+    path = 'tests/data/occupancy+detection.zip'
+    with ZipFile(path, 'r') as archive: 
+
+        header = archive.read('datatest.txt')[0:100]
+        assert b'Humidity' in header
+
+        # FIXME: using this form fails
+        #with archive.open('datatest.txt', 'r') as f:
+        #    header = f.read(100)
+        #    assert b'CO2' in header
+
 def check_roundtrip_simple(compress, level):
 
     path = 'myarchive.zip'
@@ -38,6 +51,7 @@ def check_roundtrip_simple(compress, level):
 
 def main():
     tests = [
+        test_uci_occupancy_dataset,
         test_simple_roundtrip_stored,
         test_simple_roundtrip_deflate,
     ]
